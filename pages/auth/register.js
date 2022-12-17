b8516ab2cc5b746340b577d8098ebe2c18d995a3
Pratham Sharma <prrrathm@gmail.com>
2022-12-17 14:31:49 +0530

    regex done

--- pages/auth/register.js
+++ pages/auth/register.js
@@ -6,14 +6,6 @@ import $ from "jquery";
 export default function Register() {
 	const [level, setLevel] = useState(0);
 	const [data, setData] = useState({});
-	const updateData = (vals) => {
-		var obj = Object.assign({}, data, vals);
-		setData(obj);
-		// useEffect(() => {
-		console.log(data);
-		alert(JSON.stringify(data, null, 2));
-		// }, [data]);
-	};
 	const form1 = useFormik({
 		initialValues: {
 			email: "",
@@ -32,8 +24,8 @@ export default function Register() {
 			}
 			if (!values.password) {
 				errors.password = "*Required";
-			}else if(values.password.length!=8){
-				errors.password='*Must contain 8 characters'
+			} else if (values.password.length != 8) {
+				errors.password = "*Must contain 8 characters";
 			}
 			if (!values.confirmPassword) {
 				errors.confirmPassword = "*Required";
@@ -59,9 +51,8 @@ export default function Register() {
 			const errors = {};
 			if (!values.name) {
 				errors.name = "*Required";
-			} else if (
-				!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.name)
-			) {
+			}
+			{
 				errors.name = "*invalid name";
 			}
 			if (!values.company) {
