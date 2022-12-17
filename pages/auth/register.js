7c367bc26d824bf0b236b94711cb50105401f966
Shahnwaz Khan <shahnwazkhan2007@gmail.com>
2022-12-17 14:20:48 +0530

    validation added

--- pages/auth/register.js
+++ pages/auth/register.js
@@ -23,21 +23,23 @@ export default function Register() {
 		validate: (values) => {
 			const errors = {};
 
-			// if (!values.email) {
-			// 	errors.email = "*required";
-			// } else if (
-			// 	!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-			// ) {
-			// 	errors.email = "*invalid email";
-			// }
-			// if (!values.password) {
-			// 	errors.password = "*required";
-			// }
-			// if (!values.confirmPassword) {
-			// 	errors.confirmPassword = "*Required";
-			// } else if (values.confirmPassword != values.password) {
-			// 	errors.confirmPassword = "*password doesn't match.";
-			// }
+			if (!values.email) {
+				errors.email = "*Required";
+			} else if (
+				!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+			) {
+				errors.email = "*Invalid email";
+			}
+			if (!values.password) {
+				errors.password = "*Required";
+			}else if(values.password.length!=8){
+				errors.password='*Must contain 8 characters'
+			}
+			if (!values.confirmPassword) {
+				errors.confirmPassword = "*Required";
+			} else if (values.confirmPassword != values.password) {
+				errors.confirmPassword = "*Password doesn't match.";
+			}
 			return errors;
 		},
 		onSubmit: async (values) => {
@@ -64,7 +66,7 @@ export default function Register() {
 				// 	errors.name = "*invalid name";
 			}
 			if (!values.company) {
-				errors.company = "*required";
+				errors.company = "*Required";
 			}
 			return errors;
 		},
